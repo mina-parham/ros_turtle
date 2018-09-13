8468e6111a5afebff80a822639a8b867ffde1b49
mina76 <mina.parham766@gmail.com>
2018-09-13 20:25:43 +0430

    change the CMake_list

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,5 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
 export PWD="/home/mina/Desktop/mina/build"
---
+++ build/mypack_mina/CMakeFiles/listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/listener.dir/src/listener.cpp.o"
+  "/home/mina/Desktop/mina/devel/lib/mypack_mina/listener.pdb"
+  "/home/mina/Desktop/mina/devel/lib/mypack_mina/listener"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
