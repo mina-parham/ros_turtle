7c732b19ff102ad8b86b4dc18d3e8c9c16ae0d12
mina76 <mina.parham766@gmail.com>
2018-09-13 23:01:56 +0430

    code reformed again

--- src/mypack_mina/src/listener.cpp
+++ src/mypack_mina/src/listener.cpp
@@ -4,20 +4,41 @@
 #include "geometry_msgs/Twist.h"
 #include <sstream>
 
-turtlesim::Pose position;
-void LocationOfTurtle(const turtlesim::Pose::ConstPtr& msg)
+turtlesim::Pose position1;
+turtlesim::Pose position2;
+void LocationOfTurtle1(const turtlesim::Pose::ConstPtr& msg)
 {
   
 //at first I find the position of  first turtle and print it to make sure that it works correctly!!
  ROS_INFO("i heard:[%f],[%f],[%f],[%f],[%f]\n",msg->x,msg->y,msg->theta,msg->linear_velocity,msg->angular_velocity);
 
-//now I save this number then I can pass it to the second turtle!!
-position.x=msg->x;
-position.y=msg->y;
-position.theta=msg->theta;
-position.linear_velocity=msg->linear_velocity;
-position.angular_velocity=msg->angular_velocity;
+//now I save this number for using it!
+position1.x=msg->x;
+position1.y=msg->y;
+position1.theta=msg->theta;
+position1.linear_velocity=msg->linear_velocity;
+position1.angular_velocity=msg->angular_velocity;
 }
+//then I need the location of tutrtle2
+void LocationOfTurtle2(const turtlesim::Pose::ConstPtr& msg)
+{
+  
+
+ ROS_INFO("i heard:[%f],[%f],[%f],[%f],[%f]\n",msg->x,msg->y,msg->theta,msg->linear_velocity,msg->angular_velocity);
+
+//now I save this number then I can pass it to the second turtle!!
+position2.x=msg->x;
+position2.y=msg->y;
+position2.theta=msg->theta;
+position2.linear_velocity=msg->linear_velocity;
+position2.angular_velocity=msg->angular_velocity;
+}
+//now I will calculate the distance between two turtles!
+double distance(double tur1x,double tur1y,double tur2x,double tur2y){
+
+return sqtr(pow((tur2x-tur1x),2)+pow((tur2y-tur1y),2));
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "listener");
@@ -26,7 +47,8 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
 
  
-  ros::Subscriber sub = n.subscribe("turtle1/pose", 1000, LocationOfTurtle);
+  ros::Subscriber sub1 = n.subscribe("turtle1/pose", 1000, LocationOfTurtle1);
+  ros::Subscriber sub2 = n.subscribe("turtle2/pose", 1000, LocationOfTurtle2);
 
   ros::init(argc, argv,"talker");
   ros::Publisher pub =n.advertise<geometry_msgs::Twist>("turtle2/cmd_vel",1000);
@@ -34,12 +56,14 @@ int main(int argc, char **argv)
   int count=0;
 while (ros::ok()){
 geometry_msgs::Twist pos;
-pos.linear.x=0;
+pos.linear.x=1.5*distance(position2.x,position2.y,position1.x,position2.y);
 pos.linear.y=0;
 pos.linear.z=0;
 pos.angular.x=0;
 pos.angular.y=0;
-pos.angular.z=0;
+pos.angular.z=4*(atan2(position1.y-position2.y,position1.x-position2.y)-position2.theta);
+
+
 pub.publish(pos);
 ros::spinOnce();
 loop_rate.sleep();
